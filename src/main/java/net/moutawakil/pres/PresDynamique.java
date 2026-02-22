ebf1e7dcb5beaf11500971c42b2cbfa31286d80d
Latifa Moutawakil <latefamoutawakil@gmail.com>
2026-02-22 20:36:20 +0000

    Injection des dépendances par instanciation dynamique2

--- src/main/java/net/moutawakil/pres/PresDynamique.java
+++ src/main/java/net/moutawakil/pres/PresDynamique.java
@@ -1,21 +1,28 @@
 package net.moutawakil.pres;
 
 import net.moutawakil.dao.IDao;
+import net.moutawakil.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
 /**
  * @author admin
  **/
 public class PresDynamique {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Scanner scanner = new Scanner(new File("config.txt"));
 
         String daoClassName = scanner.nextLine();
         Class cDao = Class.forName(daoClassName);
         IDao dao = (IDao) cDao.newInstance();
-        System.out.println(dao.getData());
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        System.out.println("RES="+metier.calcul());
     }
 }
